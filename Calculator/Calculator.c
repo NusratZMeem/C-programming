a2372e968ca79c6782db0c283b73e75aed7316b1
NusratZMeem <123955231+NusratZMeem@users.noreply.github.com>
2023-01-30 13:41:26 +0000

    Test

--- Calculator/Calculator.c
+++ Calculator/Calculator.c
@@ -2,7 +2,18 @@
 int main()
 {
     char operator;
+    float number1;
+    float number2;
+
     printf("enter the operator: (+ - * /)");
-    scanf("%c",&operator);
+    scanf("%c", &operator);
+
+    printf("enter the 1st number");
+    scanf("%f",&number1);
+
+    printf("enter the 2nd number");
+    scanf("%f",&number2);
+
+
     return 0;
 }
