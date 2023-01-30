0ba9d432b4dd7da1ff7de790f02af1498736f541
NusratZMeem <123955231+NusratZMeem@users.noreply.github.com>
2023-01-30 15:26:26 +0000

    done

--- Calculator/Calculator.c
+++ Calculator/Calculator.c
@@ -6,7 +6,7 @@ int main()
     float number2;
     float result;
 
-    printf("enter the operator: (sq, cube, +, -,  *,  /,)");
+    printf("enter the operator: (x, y, +, -,  *,  /,)");
     scanf("%c", &operator);
 
     printf("enter the 1st number");
@@ -30,17 +30,26 @@ result = number1*number2;
 printf("multiplication: %f",result);
 break;
 
+case '/':
+result = number1 / number2;
+printf("division: %f",result);
+break;
 
+case'x':
+result = number1*number1;
+printf("square of number 1: %f",result);
+result=number2 *number2;
+printf("square of number 2: %f",result);
+break;
 
+case'y':
+result = number1 *number1 *number1;
+printf("cube of 1st number:%f",result);
+result = number2 *number2 *number2;
+printf("cube of 2nd number:%f",result);
+break;
 
-
-
-
-
-
-
-
-    default:
+ default:
         printf("%c is invalid", operator);
         break;
     }
