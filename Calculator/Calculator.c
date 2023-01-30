25cac41f098670ab369108950b20251435b8410d
NusratZMeem <123955231+NusratZMeem@users.noreply.github.com>
2023-01-30 14:57:10 +0000

    half done

--- Calculator/Calculator.c
+++ Calculator/Calculator.c
@@ -4,16 +4,46 @@ int main()
     char operator;
     float number1;
     float number2;
+    float result;
 
-    printf("enter the operator: (+ - * /)");
+    printf("enter the operator: (sq, cube, +, -,  *,  /,)");
     scanf("%c", &operator);
 
     printf("enter the 1st number");
-    scanf("%f",&number1);
+    scanf("%f", &number1);
 
     printf("enter the 2nd number");
-    scanf("%f",&number2);
+    scanf("%f", &number2);
+    switch (operator)
+    {
+    case '+':
+        result = number1 + number2;
+        printf("Sum: %f",result);
+        break;
+case'-':
+result=number1-number2;
+printf("subtraction: %f",result);
+break;
 
+case'*':
+result = number1*number2;
+printf("multiplication: %f",result);
+break;
+
+
+
+
+
+
+
+
+
+
+
+    default:
+        printf("%c is invalid", operator);
+        break;
+    }
 
     return 0;
 }
