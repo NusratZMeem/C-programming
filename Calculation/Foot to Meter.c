c4c678cd92439a190d97152b36fad9f109d4594e
NusratZMeem <123955231+NusratZMeem@users.noreply.github.com>
2023-02-03 16:41:02 +0000

    done

---
+++ Calculation/Foot to Meter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+
+float Foot,Meter;
+printf("enter the measurement");
+scanf("%f",&Foot);
+Meter = Foot* 0.304;
+printf("Foot to Meter:%f",Meter);
+return 0 ;
+
+}
