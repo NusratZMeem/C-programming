f29ea4ff048b67eaa72f908954f7e0c5f7b870ee
NusratZMeem <123955231+NusratZMeem@users.noreply.github.com>
2023-03-03 10:26:07 +0000

    done

--- Loop/cubeforloop.c
+++ Loop/cubeforloop.c
@@ -1,17 +1,16 @@
+//Write a program in C to display the cube of the number upto given an integer
 #include<stdio.h>
 int main()
 {
     int i,m,cube;
-    printf("enter the number");
+    printf("enter the number :");
     scanf("%d",&m);
     
     for(i=1;i<=m;i++)
     {
         cube=i*i*i;
-        printf("the cube %d:%d\n",i,cube);
+        printf("the cube is %d:%d\n",i,cube);
 
     }
-
-
     return 0;
 }
